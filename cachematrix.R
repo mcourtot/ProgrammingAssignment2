07da1089c962e1f1f78f69f4a04f1ee9e5df5250
mcourtot <mcourtot@gmail.com>
2015-03-19 12:25:39 -0700

    Created functions for assignment
    Tested locally ok

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a reponse to the programming assignement 2 from the R programming course at
+## https://class.coursera.org/rprog-012/human_grading/view/courses/973493/assessments/3/submissions
+## Functions description and stubs have been reused from the course page, and code has been implemented following the examples and syntax provided
 
-## Write a short comment describing this function
 
+## Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix rather than compute it repeatedly.
+## This pair of functions allow to cache the inverse of a matrix.
+
+## makeCacheMatrix creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
+    
+        invMat <- NULL
+        set <- function(y) {
+            x <<- y
+            invMat <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) invMat <<- inverse
+        getinverse <- function() invMat
+        list(set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), then cachesolve retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invMat <- x$getinverse()
+        if(!is.null(invMat)) {
+            message("Found cached version")
+            return(invMat)
+        }
+        #if no cached version is found
+        message("No cached version")
+        data <- x$get()
+        #compute the inverse of the matrix - note we assume x to be a square invertible matrix
+        invMat <- solve(data, ...)
+        x$setinverse(invMat)
+        invMat
 }
